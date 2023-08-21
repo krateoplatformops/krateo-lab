cf46cd455c56f1f8f815cd4686e37e1f9baa38dd
Diego Braga <diego.braga86@gmail.com>
2023-08-21 13:00:21 +0200

    Adding verify script for step1

--- krateo-composable-operations/step1/verify.sh
+++ krateo-composable-operations/step1/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl wait deployment core-provider --for condition=Available=True --timeout=30s
+kubectl wait deployment core-provider --for condition=Available=True --timeout=60s --namespace krateo-system
