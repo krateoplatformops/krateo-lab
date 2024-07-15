a7afd6610c5a63ff820c0f85bfb5cf94b0c3ddac
FrancescoL96 <45236222+FrancescoL96@users.noreply.github.com>
2024-07-15 19:07:42 +0200

    fix: part of the foreground of step0 has been moved to step1 to avoid vcluster conflicts (#81)

---
+++ krateo-composable-finops/step0/vcluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
+echo "vcluster:
+  image: rancher/k3s:v1.30.2-k3s2
+  extraArgs:
+    - --kube-apiserver-arg
+    - feature-gates=CustomResourceFieldSelectors=true" > vcluster.yaml
+helm upgrade --install vcluster vcluster   --values vcluster.yaml   --repo https://charts.loft.sh   --namespace vcluster   --repository-config='' --create-namespace
+echo "Installing VCluster. Please wait..."
+kubectl rollout status --watch --timeout=600s statefulset/vcluster -n vcluster
+sleep 5s
--- krateo-composable-finops/step1/vcluster-and-webserver-mock.sh
+++ krateo-composable-finops/step1/vcluster-and-webserver-mock.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
-echo "vcluster:
-  image: rancher/k3s:v1.30.2-k3s2
-  extraArgs:
-    - --kube-apiserver-arg
-    - feature-gates=CustomResourceFieldSelectors=true" > vcluster.yaml
-helm upgrade --install vcluster vcluster   --values vcluster.yaml   --repo https://charts.loft.sh   --namespace vcluster   --repository-config='' --create-namespace
-echo "Installing VCluster. Please wait..."
-
-sleep 10s
-vcluster connect vcluster -n my-vcluster > /dev/null 2>&1  &
+vcluster connect vcluster -n vcluster > /dev/null 2>&1  &
 echo "apiVersion: v1
 kind: Namespace
 metadata:
