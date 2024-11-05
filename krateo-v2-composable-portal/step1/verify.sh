8e29d4c08ac3765be91eed3cd2e8fed4f825174c
Diego Braga <diego.braga86@gmail.com>
2024-11-05 11:30:47 +0100

    feat: update scenario to krateo v2.2

--- krateo-v2-composable-portal/step1/verify.sh
+++ krateo-v2-composable-portal/step1/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl wait krateoplatformops krateo --for condition=Ready=True --timeout=600s --namespace krateo-system
+kubectl wait krateoplatformops krateo --for condition=Ready=True --timeout=300s --namespace krateo-system
