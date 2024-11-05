e29586e5db3a2d07e98be240b483271eb9969fe2
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2024-11-05 20:53:35 +0100

    fix: namespace incoherence
    fix: namespace incoherence

--- krateo-v2-composable-operations/step4/verify.sh
+++ krateo-v2-composable-operations/step4/verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl wait fireworksapp fireworksapp-composition-2 --for condition=Ready=True --namespace krateo-system
-kubectl wait fireworksapp fireworksapp-composition-1 --for condition=Ready=True --namespace krateo-system
+kubectl wait fireworksapp fireworksapp-composition-2 --for condition=Ready=True --namespace fireworksapp-system
+kubectl wait fireworksapp fireworksapp-composition-1 --for condition=Ready=True --namespace fireworksapp-system
