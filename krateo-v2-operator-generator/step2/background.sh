ca5c00271a218019b3244a1e835085543f0971c7
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2024-11-13 18:23:09 +0100

    fix: test fix

--- krateo-v2-operator-generator/step2/background.sh
+++ krateo-v2-operator-generator/step2/background.sh
@@ -8,47 +8,8 @@ while true; do
     resource_yaml=$(kubectl get restdefinition gh-repo -n gh-system -o yaml 2> /dev/null)
 
     # Check if the krateo.io/external-create-pending annotation is present
-    if [ $? == 0 ] && ! grep -q 'krateo.io/external-create-succeeded' <<< "$resource_yaml"; then
-        echo "Removing finalizers and deleting the resource."
-
-        # Check if the finalizers are present
-        if grep -q 'finalizers' <<< "$resource_yaml"; then
-            echo "Finalizers are present, removing them."
-            # Remove the finalizers using kubectl patch
-            kubectl patch restdefinition gh-repo -n gh-system --type=json -p='[{"op":"remove","path":"/metadata/finalizers"}]' 2> /dev/null
-        fi
-
-        # Delete the resource
-        kubectl delete restdefinition gh-repo -n gh-system 2> /dev/null
-
-        # Apply the resource again
-
-        cat <<EOF | kubectl apply -f -
-kind: RestDefinition
-apiVersion: swaggergen.krateo.io/v1alpha1
-metadata:
-  name: gh-repo
-  namespace: gh-system
-spec:
-  oasPath: https://raw.githubusercontent.com/krateoplatformops/github-oas3/1c1a6332378a931b5998b00742bcfbf136601b18/repo.yaml
-  resourceGroup: gen.github.com
-  resource: 
-    kind: Repo
-    identifiers:
-      - id 
-      - name
-      - html_url
-    verbsDescription:
-    - action: create
-      method: POST
-      path: /orgs/{org}/repos
-    - action: delete
-      method: DELETE
-      path: /repos/{org}/{name}
-    - action: get
-      method: GET
-      path: /repos/{org}/{name}
-EOF
+    if [ $? == 0 ] && ! grep -q 'krateo.io/external-create-succeeded' <<< "$resource_yaml" &&  grep -q 'krateo.io/external-create-pending'; then
+        kubectl patch restdefinition gh-repo -n gh-system --type=json -p='[{"op":"remove","path":"/metadata/annotations/krateo.io~1external-create-pending"}]' 2>/dev/null
     else
         echo "The krateo.io/external-create-pending annotation is not present, not deleting the resource."
     fi
