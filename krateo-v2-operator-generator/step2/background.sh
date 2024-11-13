52f4ce8f51d437c399314cbd3009caeca1b9b9db
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2024-11-13 18:33:23 +0100

    fix: test fix (#162)

--- krateo-v2-operator-generator/step2/background.sh
+++ krateo-v2-operator-generator/step2/background.sh
@@ -8,7 +8,7 @@ while true; do
     resource_yaml=$(kubectl get restdefinition gh-repo -n gh-system -o yaml 2> /dev/null)
 
     # Check if the krateo.io/external-create-pending annotation is present
-    if [ $? == 0 ] && ! grep -q 'krateo.io/external-create-succeeded' <<< "$resource_yaml" &&  grep -q 'krateo.io/external-create-pending'; then
+    if [ $? == 0 ] && ! grep -q 'krateo.io/external-create-succeeded' <<< "$resource_yaml" &&  grep -q 'krateo.io/external-create-pending' <<< "$resource_yaml"; then
         kubectl patch restdefinition gh-repo -n gh-system --type=json -p='[{"op":"remove","path":"/metadata/annotations/krateo.io~1external-create-pending"}]' 2>/dev/null
     else
         echo "The krateo.io/external-create-pending annotation is not present, not deleting the resource."
